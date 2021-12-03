fa83abbcb8343b67eefec1693f04cf23a990f5af
Bellaabdelouahab <Abdobella977@gmail.com>
2021-12-03 20:46:40 +0100

    315

---
+++ JavaProject/src/Home/Cient_side_Form_1Controller.java
@@ -0,0 +1,17 @@
+package Home;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.controlsfx.control.Rating;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class Cient_side_Form_1Controller implements Initializable {
+    @FXML private Rating tt; 
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        
+    }
+}
