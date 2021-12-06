1bf424717d5921807805131c699c5336d1d41a1f
bouslama-hamza <ham.bousa98@gmail.com>
2021-12-06 02:17:53 +0100

    add Sign Up page

--- JavaProject/src/Main/App.java
+++ JavaProject/src/Main/App.java
@@ -21,5 +21,4 @@ public class App extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
     }
-
 }
--- JavaProject/src/controllers/LoginController.java
+++ JavaProject/src/controllers/LoginController.java
@@ -1,13 +1,40 @@
 package controllers;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class LoginController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void SwitchToLogin(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("../resources/view/LogIn.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();    
+    }
+
+    public void SwitchToSignUp(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("../resources/view/SignUp.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     private TextField email_text;
