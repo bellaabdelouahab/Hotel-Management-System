82c65786b3a31142f034bfcd6356fffa1cba19f3
yassineboujrada <yassine.boujrada@gmail.com>
2021-12-07 10:29:21 +0100

    conditions

--- JavaProject/src/DataBaseConnection.java
+++ JavaProject/src/DataBaseConnection.java
@@ -1,23 +1,46 @@
-
 import java.sql.*;
 
 public class DataBaseConnection {
     public static void main(String[] args) {
-        Connection con = null;
+        String db = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username = "hotel_bd";
+
+        String password = "hotel";// "insert into employee values (1,'admin','hotel',null,null,null,'admin')",
+        String[] j = {
+                "insert into compte_employee values (1,'yassine@gmail.com','yassine',2)",
+                "insert into compte_employee values (2,'bella@gmail.com','abdelwahab',3)",
+                "insert into compte_employee values (3,'bousslama@gmail.com','hamza',4)",
+                "insert into compte_employee values (4,'el_bazzi@gmail.com','hiba',5)"
+        };
+
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            // String server = "localhost";// "DESKTOP-SMO7HS9";
-            // String server_port = "1521";
-            //String sid = "orcl";
-            //String url = "jdbc:oracle:thin:@" + server + ":" + server_port + ":orcl";
-            con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.192:1521/orcl","yassine", "yassine2002");
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from e");
-            while (rs.next()) {
-                System.out.println("no_etude \t" + rs.getInt(1));
-                System.out.println("sigle \t" + rs.getString(2));
+            Connection connection = DriverManager.getConnection(db, username, password);
+            Statement statement = connection.createStatement();
+
+            int rest = statement
+                    .executeUpdate("insert into compte_employee values (4,'el_bazzi@gmail.com','hiba',5)");
+            if (rest > 0) {
+                System.out.println("its working");
             }
-            con.close();
+
+            // ResultSet rs = st.executeQuery(query);
+            // try {
+            // while (rs.next()) {
+            // System.out.println("id \t" + rs.getInt(1));
+            // System.out.println("no_etude \t" + rs.getString(2));
+            // System.out.println("sigle \t" + rs.getString(3));
+            // System.out.println("sigle \t" + rs.getString(4));
+            // System.out.println("sigle \t" + rs.getInt(5));
+            // System.out.println("sigle \t" + rs.getInt(6));
+            // System.out.println("sigle \t" + rs.getString(7));
+            // }
+            // } catch (Exception e) {
+            // System.out.println("wtffffffffffffffff");
+            // }}
+
+            statement.close();
+            connection.close();
         } catch (Exception e) {
             System.out.println("dor tqwd2 " + e);
         }
