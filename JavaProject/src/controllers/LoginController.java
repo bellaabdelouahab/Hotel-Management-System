5a58f7dc80032260923f9cc93c955320eff87630
yassineboujrada <yassine.boujrada@gmail.com>
2021-12-07 20:27:26 +0100

    hello mf

--- JavaProject/src/controllers/LoginController.java
+++ JavaProject/src/controllers/LoginController.java
@@ -13,9 +13,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Line;
@@ -24,28 +26,6 @@ import javafx.util.Duration;
 import javafx.scene.Node;
 
 public class LoginController {
-
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    // public void SwitchToLogin(ActionEvent event) throws IOException {
-    // root =
-    // FXMLLoader.load(getClass().getResource("../resources/view/LogIn.fxml"));
-    // stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    // scene = new Scene(root);
-    // stage.setScene(scene);
-    // stage.show();
-    // }
-
-    // public void SwitchToSignUp(ActionEvent event) throws IOException {
-    // root =
-    // FXMLLoader.load(getClass().getResource("../resources/view/SignUp.fxml"));
-    // stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    // scene = new Scene(root);
-    // stage.setScene(scene);
-    // stage.show();
-    // }
 
     @FXML
     private TextField email_text;
@@ -75,6 +55,12 @@ public class LoginController {
 
     @FXML
     private Button signup_btn;
+    @FXML
+    private AnchorPane achnopane;
+    @FXML
+    private Label pass_word;
+    @FXML
+    private Label emai_label;
 
     // hover de button sign in
     @FXML
@@ -134,9 +120,7 @@ public class LoginController {
     // login
     @FXML
     public void login_formule(ActionEvent event) {
-        // System.out.println(email_text.getText().contains("@gmail.com") + "\t" +
-        // password_label.getText().length());
-        String x = "";
+        String x = "",y="";
         if (email_text.getText().contains("@gmail.com") && password_label.getText().length() != 8) {
             try {
                 Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -147,36 +131,41 @@ public class LoginController {
                 ResultSet rs = st.executeQuery("select email,password from compte_employee where lower(email)='"
                         + email_text.getText().toLowerCase() + "'");
                 while (rs.next()) {
-                    // System.out.println("no_etude \t" + rs.getString(1));
                     System.out.println(
                             "\t" + rs.getString(2).toLowerCase() + "\t" + password_label.getText().toLowerCase());
                     x = rs.getString(2).toLowerCase();
-                    // System.out.println(x.equals(password_label.getText()));
+                    y=rs.getString(1).toLowerCase();
                 }
-                if (x.equals(password_label.getText())) {
-                    try {
-                        System.out.println("abdo");
-                        Parent root = FXMLLoader.load(getClass().getResource("../resources/view/SignUp.fxml"));
-                        Scene scene = signin_btn.getScene();
-                        root.translateXProperty().set(scene.getWidth());
-                        StackPane achnopane = (StackPane) scene.getRoot();// .getChildren().add(root);
-                        Timeline timeline = new Timeline();
-                        KeyValue kv = new KeyValue(root.translateXProperty(), 0, Interpolator.EASE_IN);
-                        KeyValue kv1 = new KeyValue(general_pane.translateXProperty(),
-                                -(root.translateXProperty().get()), Interpolator.EASE_IN);
-                        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
-                        KeyFrame kf1 = new KeyFrame(Duration.seconds(1), kv1);
-                        timeline.getKeyFrames().add(kf);
-                        timeline.getKeyFrames().add(kf1);
-                        timeline.setOnFinished(t -> {
-                            achnopane.getChildren().remove(general_pane);
-                        });
-                        timeline.play();
-                    } catch (Exception e) {
-                        System.out.println(e);
+                if (y.equals(email_text.getText())) {
+                    
+                    if (x.equals(password_label.getText())) {
+                        try {
+                            Parent root = FXMLLoader.load(getClass().getResource("../resources/view/Profile.fxml"));
+                            Scene scene = signin_btn.getScene();
+                            root.translateXProperty().set(scene.getWidth());
+                            achnopane.getChildren().add(root);
+                            Timeline timeline = new Timeline();
+                            KeyValue kv = new KeyValue(root.translateXProperty(), 0, Interpolator.EASE_IN);
+                            KeyValue kv1 = new KeyValue(general_pane.translateXProperty(),
+                                    -(root.translateXProperty().get()), Interpolator.EASE_IN);
+                            KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+                            KeyFrame kf1 = new KeyFrame(Duration.seconds(1), kv1);
+                            timeline.getKeyFrames().add(kf);
+                            timeline.getKeyFrames().add(kf1);
+                            timeline.setOnFinished(t -> {
+                                achnopane.getChildren().remove(general_pane);
+                            });
+                            timeline.play();
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                    } else {
+                        pass_word.setStyle("-fx-text-fill:red;");
+                        pass_line.setStyle("-fx-stroke:red;");
                     }
-                } else {
-                    System.out.println("mmmm");
+                }else{
+                    emai_label.setStyle("-fx-text-fill:red;");
+                    email_line.setStyle("-fx-stroke:red;");
                 }
 
                 st.close();
