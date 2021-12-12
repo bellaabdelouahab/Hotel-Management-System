a0b8e8d60a44d3c7117f417fa186f63d37cb2ff0
bouslama-hamza <ham.bousa98@gmail.com>
2021-12-12 02:40:02 +0100

    add admin profile

---
+++ JavaProject/src/Controllers/Admin/Functions/ProfileController.java
@@ -0,0 +1,59 @@
+package Controllers.Admin.Functions;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class ProfileController implements Initializable{
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    @FXML
+    private VBox AdminMenu;
+
+    @FXML
+    void HideMenuBar(MouseEvent event) {
+        AdminMenu.setVisible(false);
+    }
+
+    @FXML
+    void LogOut(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../../../Resources/VIEW/Admin/Authentification/Login.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void ShowMenuBar(MouseEvent event) {
+        AdminMenu.setVisible(true);
+    }
+
+    @FXML
+    void SwitchToUser(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../../../Resources/VIEW/Admin/Functions/User.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        AdminMenu.setVisible(false);
+    }
+    
+}
--- JavaProject/src/Main/App.java
+++ JavaProject/src/Main/App.java
@@ -3,7 +3,6 @@ package Main;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,9 +14,9 @@ public class App extends Application {
     // Fonction Of The Main App
     @Override
     public void start(Stage primaryStage) throws IOException {
+
         Properties Prop = new Properties();
-        System.out.println(System.getProperty("user.dir")+"\\src\\Config.properties");
-        FileInputStream config = new FileInputStream(System.getProperty("user.dir")+"\\src\\Config.properties");
+        FileInputStream config = new FileInputStream(System.getProperty("user.dir")+"/src/Config.properties");
         Prop.load(config);
         Parent root = FXMLLoader.load(App.class.getResource("../Resources/VIEW/FirstPage.fxml"));
 
@@ -30,10 +29,12 @@ public class App extends Application {
         else{
             root = FXMLLoader.load(App.class.getResource("../Resources/VIEW/Admin/Authentification/Login.fxml"));
         }
+
         Scene scene = new Scene(root);
         primaryStage.setTitle("Hotel BBBE");
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     // Launch The Main App
