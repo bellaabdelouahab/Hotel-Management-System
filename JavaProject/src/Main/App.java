fad0a2a1d5f2dc4942d835cd0c20bde03a51cdb3
yassineboujrada <yassine.boujrada@gmail.com>
2021-12-12 18:12:00 +0100

    rr

--- JavaProject/src/Main/App.java
+++ JavaProject/src/Main/App.java
@@ -1,8 +1,9 @@
-package Main;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,9 +15,9 @@ public class App extends Application {
     // Fonction Of The Main App
     @Override
     public void start(Stage primaryStage) throws IOException {
-
         Properties Prop = new Properties();
-        FileInputStream config = new FileInputStream(System.getProperty("user.dir")+"/src/Config.properties");
+        System.out.println(System.getProperty("user.dir")+"\\src\\Config.properties");
+        FileInputStream config = new FileInputStream(System.getProperty("user.dir")+"\\src\\Config.properties");
         Prop.load(config);
         Parent root = FXMLLoader.load(App.class.getResource("../Resources/VIEW/FirstPage.fxml"));
 
@@ -27,14 +28,12 @@ public class App extends Application {
             root = FXMLLoader.load(App.class.getResource("../Resources/VIEW/Employer/HomePage.fxml"));
         }
         else{
-            root = FXMLLoader.load(App.class.getResource("../Resources/VIEW/Admin/Functions/Rooms.fxml"));
+            root = FXMLLoader.load(App.class.getResource("../Resources/VIEW/Admin/Authentification/Login.fxml"));
         }
-
         Scene scene = new Scene(root);
         primaryStage.setTitle("Hotel BBBE");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     // Launch The Main App
--- JavaProject/src/Main/User.java
+++ JavaProject/src/Main/User.java
@@ -1,4 +1,4 @@
-package Main;
+//package Main;
 
 public class User {
     private int Id;
